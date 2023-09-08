aa8f57d7235ad5cd83bb688d22520979e1829bf4
Preetichandrasekaran <144321011+Preetichandrasekaran@users.noreply.github.com>
2023-09-08 23:19:46 +0530

    Revert "A added fewlines"

--- src/org/git/GitProgram.java
+++ src/org/git/GitProgram.java
@@ -1,16 +1,9 @@
 package org.git;
 
 public class GitProgram {
-	
-	private void testCase() {
-		System.out.println("TestCase:1");
-
-	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("TestCase");
+		System.out.println("TestCase1");
 	}
-	
-	
 }
