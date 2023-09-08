e7a96b0739aea9c8e8b00bd7431067dcbdd2e32c
preetichandrasekaran <preetichandrasekaran@gmail.com>
2023-09-08 22:38:53 +0530

    A added fewlines

--- src/org/git/GitProgram.java
+++ src/org/git/GitProgram.java
@@ -1,9 +1,16 @@
 package org.git;
 
 public class GitProgram {
+	
+	private void testCase() {
+		System.out.println("TestCase:1");
+
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("TestCase1");
+		System.out.println("TestCase");
 	}
+	
+	
 }
